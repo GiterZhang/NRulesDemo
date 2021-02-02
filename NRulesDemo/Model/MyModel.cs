1f28383ee028be659f1d9b40ea883322979d2e53
zzy <1203675375@qq.com>
2021-02-02 22:32:26 +0800

    添加项目文件。

---
+++ NRulesDemo/Model/MyModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRulesDemo.Model
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public bool IsPreferred { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void NotifyAboutDiscount()
+        {
+            Console.WriteLine($"Customer {Name} was notified about a discount");
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; }
+        public Customer Customer { get; }
+        public int Quantity { get; }
+        public double UnitPrice { get; }
+        public double PercentDiscount { get; set; }
+        public bool IsOpen { get; set; } = true;
+
+        public Order(int id, Customer customer, int quantity, double unitPrice)
+        {
+            Id = id;
+            Customer = customer;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+}
